c8b7bb843ccb520007ea48df16b38be7dc1d6db6
ThisFellowAintMellow <danielgreetham@gmail.com>
2018-08-11 15:20:30 +0100

    Added scripting for linear enemy

---
+++ Unity/Assets/Linear_enemy_movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+ public float speed;
+ 
+
+    private bool MovingRight = true;
+
+    public transform WallDetection;
+
+    void update() {
+
+        transform.translate(vector2.right * speed * time.deltatime);
+
+        RaycastHit2D WallInfo = Physics2D.Raycast(WallDetection.position, Vector2.right, 2f);
+        if(WallInfo.collider == true)
+        {
+            if (MovingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                MovingRight = false;
+            }
+            else
+            { transform.eulerAngles = new Vector3(0, 0, 0);
+            movingright = true
+            }
+        }
+	}
+}
