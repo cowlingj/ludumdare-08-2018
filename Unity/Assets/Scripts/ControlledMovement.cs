9b8d27b9e7e81c924fdd11efff6da66c97411ad8
Jonathan Cowling <09cowlingj@gmail.com>
2018-08-11 14:59:46 +0100

    Add player movement script

---
+++ Unity/Assets/Scripts/ControlledMovement.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ControlledMovement : Movement {
+
+
+	void FixedUpdate() {
+    //CrossPlatformInput.GetAxis('horrizontal')
+  }
+}
---
+++ Unity/Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public bool canJump;
+  public float groundSpeed;
+	public float airSpeed;
+	public float jumpThreshold;
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void moveX(float val) {
+		float speed = Physics.Raycast(transform.position, -Vector3.up, jumpThreshold)? groundSpeed : airSpeed;
+    rb.AddForce(new Vector3(val * speed, 0, 0));
+	}
+
+	void moveY(float val) {
+		if (!canJump) {
+			return;
+		}
+		float speed = Physics.Raycast(transform.position, -Vector3.up, jumpThreshold)? groundSpeed : airSpeed;
+		rb.AddForce(new Vector3(0, val * speed, 0));
+	}
+}
